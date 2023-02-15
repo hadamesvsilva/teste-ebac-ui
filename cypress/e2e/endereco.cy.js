d680602daa9ee0273737714f524f251b462b96aa
hadamesvsilva <ulttrrass@gmail.com>
2023-02-15 13:20:26 -0300

    Usando arquivos de dados - aula8mod12

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -9,7 +11,25 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-        //cadastro de endereço
+        EnderecoPage.editarEnderecoFaturamento('Mateus', 'Moreira', 'AB MODA', 'Brasil', 'Av. Bagdá', '999', 'Belo Horizonte', 'Minas Gerais', '66542-896', '3198855-9632', 'abdulhanasha@kabul.com.br')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivos de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
 });
